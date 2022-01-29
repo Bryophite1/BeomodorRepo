2164ff9f745ae303eb08d11de339cc0dbff448dd
Bryophite1 <76822050+Bryophite1@users.noreply.github.com>
2022-01-28 23:02:29 -0500

    More Tinkering, Smoke added, better particles

--- Beomodor/Assets/Scripts/BeeFollowScript.cs
+++ Beomodor/Assets/Scripts/BeeFollowScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class BeeFollowScript : MonoBehaviour
 {
     private GameObject player;
+    public ParticleSystem beeParticle;
 
     public float beeSpeed;
+    public bool sleeping; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +17,31 @@ public class BeeFollowScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (sleeping)
+        {
+            transform.position = transform.position + Vector3.down * beeSpeed * Time.deltaTime;
+        }
     }
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("SingRadius"))
+        if (other.CompareTag("SingRadius") && !sleeping)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, beeSpeed * Time.deltaTime);
         }
+        if (other.CompareTag("Smoke"))
+        {
+            Debug.Log("Smoked");
+            StartCoroutine(Sleep());
+        }
+    }
+    public IEnumerator Sleep()
+    {
+        yield return new WaitForSeconds(0.2f);
+        sleeping = true;
+        beeParticle.gravityModifier = 0.5f;
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObject);
     }
 }
