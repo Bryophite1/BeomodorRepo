dafdee4addd2a8c58dd0df09db90db0b3be33f92
Justin-Tiger <justinmeyer914@gmail.com>
2022-02-11 15:43:01 -0500

    Finished Title page

--- Beomodor/Assets/Scripts/Title.cs
+++ Beomodor/Assets/Scripts/Title.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Title : MonoBehaviour
     public GameObject HTP_page;
     public GameObject cred_page;
     public bool isActive;
+    public bool sceneChange;
 
 
     // Start is called before the first frame update
@@ -30,7 +32,11 @@ public class Title : MonoBehaviour
 	if (Input.GetKeyDown(KeyCode.Space)) {
 	    if (transform.position.x == -7.5)
             {
-                print("Start");
+            	int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            	if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+            	{
+                	SceneManager.LoadScene(nextSceneIndex);
+            	}
             }
 	    if (transform.position.x == -2.5)
             {
